962e01ae571a42426f9344e412b561f4b1c5f194
bsukalo <bsukalo@outlook.com>
2024-08-01 00:38:55 +0200

    Create error handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/connectDb");
 const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
 
 connectDb();
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use("/api", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () =>
 	console.log(`Server is running on http://localhost:${port}`)
