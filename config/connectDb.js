486162dedee8aeb3b631e62f39788a840621f469
bsukalo <bsukalo@outlook.com>
2024-08-02 04:06:13 +0200

    Connect MongoDB

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
 	try {
-		await mongoose.connect(process.env.URI);
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("Connected to database");
 	} catch (error) {
 		console.log("Could not connect to database:", error);
