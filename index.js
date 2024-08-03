fae16393339d79f8fd99720ba1065b27c147d811
bsukalo <bsukalo@outlook.com>
2024-08-03 03:19:23 +0200

    Set up CORS

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const connectDb = require("./config/connectDb");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 
 connectDb();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+const corsOptions = {
+	origin: "http://localhost:5173",
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	credentials: true,
+	optionsSuccessStatus: 204,
+};
+
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api", require("./routes/userRoutes"));
